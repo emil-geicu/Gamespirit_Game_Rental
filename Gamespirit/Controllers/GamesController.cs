fc8fdda9fdb4be6f7c8fc65c0286ddf2ed76f61e
EG\emilgeicu <emilgeicu@gmail.com>
2023-03-11 18:53:55 +0200

    Created_Games_and_Players_pages

---
+++ Gamespirit/Controllers/GamesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gamespirit.Controllers
+{
+    public class GamesController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public GamesController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
